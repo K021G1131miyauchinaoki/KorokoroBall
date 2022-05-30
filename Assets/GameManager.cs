a38aa4d920affd82bb2474692f4a0fc8d486a628
naoki147 <k021g1131@g.neec.ac.jp>
2022-05-31 08:18:34 +0900

    06完了

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    private int coinCount;
+    public Text textComponent;
+
+    private void Start()
+    {
+        Screen.SetResolution(1920, 1080, false);
+        Application.targetFrameRate = 60;
+        coinCount = 0;
+    }
     public  void    ChangeScene(string  nextScene)
     {
         SceneManager.LoadScene(nextScene);
     }
+
+    public  void    AddCoinCount()
+    {
+        coinCount = coinCount + 1;
+        Debug.Log("CoinCount:" + coinCount);
+        textComponent.text = "CoinCount:" + coinCount;
+    }
+
+
 }
